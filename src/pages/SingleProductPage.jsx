e8e5a5fb89d7046d2ad09077e09afca9d8d25dcd
AlbertLeipzig <albertclemvill@gmail.com>
2023-10-19 12:03:47 +0200

    finished

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,7 +1,14 @@
+import { useParams } from 'react-router-dom';
+import productsData from '../localData/productsData.json';
+
 export const SingleProductPage = () => {
+  const { productId } = useParams();
+  const product = productsData.find((product) => product.id === Number(productId));
   return (
     <div className="single-product-page">
-      <h1>Single Product Page</h1>
+      <h1>{product.name}</h1>
+      <p>Price : {product.price}</p>
+      <p>Brand : {product.brand}</p>
     </div>
   );
 };
